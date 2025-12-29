fb7bdf37c220095d269953934918bff680dea321
Soufiane EL KASSIB <elkassib.soufiane@gmail.com>
2025-12-29 18:44:52 +0100

    Updates

--- server/entry.mjs
+++ server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Bn0gmZsf.mjs';
-import { manifest } from './manifest_CcXoNGio.mjs';
+import { manifest } from './manifest_BMbuI_R8.mjs';
 
 const serverIslandMap = new Map();;
 
