9ec84930cccd417325951c4a7eb3977953488b36
Soufiane EL KASSIB <elkassib.soufiane@gmail.com>
2026-02-15 23:35:30 +0000

    Updates

---
+++ _astro/Alert3-E3X2OWZV.BqwafYJL.js
@@ -0,0 +1,6 @@
+import{e as r,g as o,j as e,S as a,X as c}from"./registries-7DBE654J.C6nm9Ta7.js";import{r as l}from"./index.DQDNfAS8.js";/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 16v-4",key:"1dtifu"}],["path",{d:"M12 8h.01",key:"e9boi3"}]],x=r("info",d);var b=({data:s})=>{const i=o(s.type,s.content),[t,n]=l.useState(!0);return t?e.jsx(a,{id:s.id,design:s.design,className:"py-3 px-4 border-b border-border",children:e.jsxs("div",{className:"flex items-center gap-3 container mx-auto max-w-5xl",children:[e.jsx(x,{size:20,className:"shrink-0"}),e.jsxs("div",{className:"flex-1 text-sm",children:[e.jsx("span",{className:"font-bold",children:i.heading}),i.description&&e.jsx("span",{className:"mx-2 opacity-80",children:i.description})]}),i.dismissable!==!1&&e.jsx("button",{onClick:()=>n(!1),className:"p-1 rounded-full hover:bg-black/5 dark:hover:bg-white/10 transition-colors",children:e.jsx(c,{size:16})})]})}):null};export{b as Alert3};
--- _astro/client.BfuZpD43.js
+++ _astro/client.BfuZpD43.js
@@ -1,4 +1,4 @@
-import{b as Rh,a as jh,r as na}from"./index.DR626fqj.js";var ti={exports:{}},me={},ai={exports:{}},ui={};/**
+import{a as Rh,b as jh,r as na}from"./index.DQDNfAS8.js";var ti={exports:{}},me={},ai={exports:{}},ui={};/**
  * @license React
  * scheduler.production.js
  *
